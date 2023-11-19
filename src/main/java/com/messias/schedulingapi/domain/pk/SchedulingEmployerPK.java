cd4c8e06eae7883f6d877ab6ccad3eb82129604e
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2023-11-19 19:43:41 -0300

    creating helper class for manytomany relationship

---
+++ src/main/java/com/messias/schedulingapi/domain/pk/SchedulingEmployerPK.java
@@ -0,0 +1,53 @@
+package com.messias.schedulingapi.domain.pk;
+
+import com.messias.schedulingapi.domain.Employer;
+import com.messias.schedulingapi.domain.Scheduling;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Objects;
+
+@Embeddable
+@Data
+public class SchedulingEmployerPK {
+    @ManyToOne
+    @JoinColumn(name = "id_Scheduling")
+    private Scheduling scheduling;
+    @ManyToOne
+    @JoinColumn(name = "id_employer")
+    private Employer employer;
+
+
+    public Scheduling getScheduling() {
+        return scheduling;
+    }
+
+    public void setScheduling(Scheduling scheduling) {
+        this.scheduling = scheduling;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SchedulingEmployerPK that = (SchedulingEmployerPK) o;
+        return Objects.equals(scheduling, that.scheduling) && Objects.equals(employer, that.employer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(scheduling, employer);
+    }
+}
