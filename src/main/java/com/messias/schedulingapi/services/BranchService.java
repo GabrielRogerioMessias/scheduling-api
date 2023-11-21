2c98d87ed7e7ebb921672b45b4f08b734fcf4c7c
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2023-11-20 22:26:02 -0300

    implementation of Branch class services

--- src/main/java/com/messias/schedulingapi/services/BranchService.java
+++ src/main/java/com/messias/schedulingapi/services/BranchService.java
@@ -1,7 +1,12 @@
 package com.messias.schedulingapi.services;
 
+import com.messias.schedulingapi.domain.Branch;
 import com.messias.schedulingapi.repositories.BranchRepository;
+import jakarta.servlet.http.PushBuilder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BranchService {
@@ -10,4 +15,38 @@ public class BranchService {
     public BranchService(BranchRepository branchRepository) {
         this.branchRepository = branchRepository;
     }
+    //  insert,
+
+    public List<Branch> findAll() {
+        return branchRepository.findAll();
+    }
+
+    public Branch findById(Integer idBranch) {
+        //[exception id not found]
+        Optional<Branch> branchOptional = branchRepository.findById(idBranch);
+        return branchOptional.get();
+    }
+
+    public void delete(Integer idBranch) {
+        //[exception id not found]
+        Optional<Branch> branchOptional = branchRepository.findById(idBranch);
+        branchRepository.delete(branchOptional.get());
+    }
+
+    public Branch insert(Branch branch) {
+        return branchRepository.save(branch);
+    }
+
+    public Branch update(Integer idBranch, Branch newBranch) {
+        Optional<Branch> branchOptional = branchRepository.findById(idBranch);
+        Branch branchOld = branchOptional.get();
+        this.updateDate(branchOld, newBranch);
+        return branchRepository.save(branchOld);
+    }
+
+    public void updateDate(Branch oldBranch, Branch newBranch) {
+        oldBranch.setNameBranch(newBranch.getNameBranch());
+        oldBranch.setCity(newBranch.getCity());
+    }
+
 }
