d9dd0d655cbf6e467e0e173c1c571e5079dada53
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2023-12-18 22:53:27 -0300

    creating controller for type scheduling

--- src/main/java/com/messias/schedulingapi/controllers/TypeSchedulingController.java
+++ src/main/java/com/messias/schedulingapi/controllers/TypeSchedulingController.java
@@ -1,17 +1,57 @@
 package com.messias.schedulingapi.controllers;
 
+import com.messias.schedulingapi.domain.Employer;
+import com.messias.schedulingapi.domain.TypeScheduling;
 import com.messias.schedulingapi.services.SchedulingService;
+import com.messias.schedulingapi.services.TypeSchedulingService;
 import com.messias.schedulingapi.services.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/type-schedulings")
 public class TypeSchedulingController {
 
-    private SchedulingService schedulingService;
+    private TypeSchedulingService typeSchedulingService;
 
-    public TypeSchedulingController(SchedulingService schedulingService) {
-        this.schedulingService = schedulingService;
+    public TypeSchedulingController(TypeSchedulingService typeSchedulingService) {
+        this.typeSchedulingService = typeSchedulingService;
+    }
+
+
+    @DeleteMapping(value = "/{idTypeScheduling}")
+    public ResponseEntity<Void> delete(@PathVariable Integer idTypeScheduling) {
+        typeSchedulingService.delete(idTypeScheduling);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TypeScheduling>> findAll() {
+        List<TypeScheduling> typeSchedulingList = typeSchedulingService.findAll();
+        return ResponseEntity.ok().body(typeSchedulingList);
+    }
+
+    @GetMapping(value = "/{idTypeScheduling}")
+    public ResponseEntity<TypeScheduling> findById(@PathVariable Integer idTypeScheduling) {
+        TypeScheduling typeScheduling = typeSchedulingService.findById(idTypeScheduling);
+        return ResponseEntity.ok().body(typeScheduling);
+    }
+
+    @PostMapping
+    public ResponseEntity<TypeScheduling> insert(@RequestBody TypeScheduling typeScheduling) {
+        TypeScheduling newTypeScheduling = typeSchedulingService.insert(typeScheduling);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/id").buildAndExpand(newTypeScheduling.getIdTypeScheduling()).toUri();
+        return ResponseEntity.created(uri).body(newTypeScheduling);
+    }
+
+    @PutMapping(value = "/{idTypeScheduling}")
+    public ResponseEntity<TypeScheduling> update(@PathVariable Integer idTypeScheduling, @RequestBody TypeScheduling updateTypeScheduling) {
+        typeSchedulingService.update(idTypeScheduling, updateTypeScheduling);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- src/main/java/com/messias/schedulingapi/services/TypeSchedulingService.java
+++ src/main/java/com/messias/schedulingapi/services/TypeSchedulingService.java
@@ -1,7 +1,13 @@
 package com.messias.schedulingapi.services;
 
+import com.messias.schedulingapi.domain.Employer;
+import com.messias.schedulingapi.domain.TypeScheduling;
+import com.messias.schedulingapi.repositories.EmployerRepository;
 import com.messias.schedulingapi.repositories.TypeSchedulingRepository;
+import com.messias.schedulingapi.services.exceptionsServices.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TypeSchedulingService {
@@ -10,4 +16,36 @@ public class TypeSchedulingService {
     public TypeSchedulingService(TypeSchedulingRepository typeSchedulingRepository) {
         this.typeSchedulingRepository = typeSchedulingRepository;
     }
+
+
+    public void delete(Integer idTypeScheduling) {
+        TypeScheduling employer = typeSchedulingRepository.findById(idTypeScheduling).orElseThrow(() -> new ResourceNotFoundException(idTypeScheduling));
+        typeSchedulingRepository.delete(employer);
+    }
+
+    public List<TypeScheduling> findAll() {
+        return typeSchedulingRepository.findAll();
+
+    }
+
+    public TypeScheduling findById(Integer idTypeScheduling) {
+        return typeSchedulingRepository.findById(idTypeScheduling).orElseThrow(() -> new ResourceNotFoundException(idTypeScheduling));
+    }
+
+    public TypeScheduling insert(TypeScheduling newTypeScheduling) {
+        return typeSchedulingRepository.save(newTypeScheduling);
+    }
+
+
+    public TypeScheduling update(Integer idTypeScheduling, TypeScheduling updateTypeScheduling) {
+        TypeScheduling oldTypeScheduling = typeSchedulingRepository.findById(idTypeScheduling).orElseThrow(() -> new ResourceNotFoundException(idTypeScheduling));
+        updateData(oldTypeScheduling, updateTypeScheduling);
+        return typeSchedulingRepository.save(oldTypeScheduling);
+    }
+
+    public void updateData(TypeScheduling oldTypeScheduling, TypeScheduling updateTypeScheduling) {
+        oldTypeScheduling.setDescription(updateTypeScheduling.getDescription());
+    }
+
+
 }
