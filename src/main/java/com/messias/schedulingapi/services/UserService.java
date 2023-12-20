380f916b860d5cd986eaddb4f8f61788820e3c63
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2023-12-19 21:30:20 -0300

    implementing services for users

--- src/main/java/com/messias/schedulingapi/services/UserService.java
+++ src/main/java/com/messias/schedulingapi/services/UserService.java
@@ -1,7 +1,12 @@
 package com.messias.schedulingapi.services;
 
+import com.messias.schedulingapi.domain.Branch;
+import com.messias.schedulingapi.domain.User;
 import com.messias.schedulingapi.repositories.UserRepository;
+import com.messias.schedulingapi.services.exceptionsServices.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -9,5 +14,32 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Integer idUser) {
+        return userRepository.findById(idUser).orElseThrow(() -> new ResourceNotFoundException(idUser));
+    }
+
+    public void delete(Integer idUser) {
+        User user = userRepository.findById(idUser).orElseThrow(() -> new ResourceNotFoundException(idUser));
+        userRepository.delete(user);
+    }
+
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(Integer idUser, User updateUser) {
+        User oldUser = userRepository.findById(idUser).orElseThrow(() -> new ResourceNotFoundException(idUser));
+        this.updateDate(oldUser, updateUser);
+        return userRepository.save(oldUser);
+    }
+
+    public void updateDate(User oldUser, User updateUser) {
+        oldUser.setName(updateUser.getName());
     }
 }
