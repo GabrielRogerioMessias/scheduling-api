e5230f2b7b85770f6cbc802c5f5735448e61a99c
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-01-03 21:09:25 -0300

    implementing method in user service

--- src/main/java/com/messias/schedulingapi/repositories/UserRepository.java
+++ src/main/java/com/messias/schedulingapi/repositories/UserRepository.java
@@ -2,8 +2,12 @@ package com.messias.schedulingapi.repositories;
 
 import com.messias.schedulingapi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User findByUsername(@Param(value = "username") String username);
 }
