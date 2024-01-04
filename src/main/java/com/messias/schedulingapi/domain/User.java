fdbc7a89abab69593b24fbdfbd4e32e1bd4f6a58
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-01-03 21:05:22 -0300

    adjusting UserDetails methods

--- src/main/java/com/messias/schedulingapi/domain/User.java
+++ src/main/java/com/messias/schedulingapi/domain/User.java
@@ -2,21 +2,24 @@ package com.messias.schedulingapi.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
 
 @Entity
 @Table(name = "tb_User")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String fullName;
-    private String login;
+    private String username;
     private String password;
     @Column(name = "account_non_expired")
     private Boolean accountNonExpired;
@@ -24,6 +27,7 @@ public class User {
     private Boolean accountNonLocked;
     @Column(name = "credentials_non_expired")
     private Boolean credentialsNonExpired;
+    private Boolean enabled;
     //quando o user for carregado, também carregara de forma automatica suas permissões
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_permission", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_permission"))
@@ -37,6 +41,17 @@ public class User {
     private List<SchedulingEmployer> schedulingEmployerList = new ArrayList<>();
 
     public User() {
+    }
+
+    public User(Integer id, String fullName, String username, String password, Boolean accountNonExpired, Boolean accountNonLocked, Boolean credentialsNonExpired, Boolean enabled) {
+        this.id = id;
+        this.fullName = fullName;
+        this.username = username;
+        this.password = password;
+        this.accountNonExpired = accountNonExpired;
+        this.accountNonLocked = accountNonLocked;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.enabled = enabled;
     }
 
     public Integer getId() {
@@ -55,16 +70,42 @@ public class User {
         this.fullName = fullName;
     }
 
-    public String getLogin() {
-        return login;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
     }
 
     public void setPassword(String password) {
@@ -93,6 +134,14 @@ public class User {
 
     public void setCredentialsNonExpired(Boolean credentialsNonExpired) {
         this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
     }
 
     public List<Permission> getPermission() {
